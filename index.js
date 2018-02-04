52fba1ed99a9dabee02001482f31f1825323d82b
vasyareper <36129275+vasyareper@users.noreply.github.com>
2018-02-04 23:04:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const TOKEN = '526966802:AAEm_el5nQz50q-ldjIvqD2utNIORLvvzt4'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello, bot says: "hi, ${msg.from.first_name}"')
+const KB = {
+    vybor: 'Выбор города',
+    pomosh: 'Поддержка',
+    soc: 'Оставить отзыв'
+}
+bot.onText(/\/start/, msg => {
+    const text = 'Добро пожаловать в магазин vasyareper! Выберите город:'
+
+    bot.sendMessage(msg.chat.id, text, {
+        reply_markup: {
+            keyboard: [
+                [KB.vybor, KB.pomosh, KB.soc]
+            ]
+        }
+    }
+
+})
+
+bot.on('message', msg=> {
+
+    switch (msg.text) {
+        case KB.vybor:
+            break
+        case KB.pomosh:
+            break
+        case KB.soc:
+            break
+    }
 })
