e55eeece21f86eaaa8b5c49d32086cd1174d1b7a
vasyareper <36129275+vasyareper@users.noreply.github.com>
2018-02-05 21:02:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api')
 
-const token = '526966802:AAEm_el5nQz50q-ldjIvqD2utNIORLvvzt4';
+const TOKEN = '526966802:AAEm_el5nQz50q-ldjIvqD2utNIORLvvzt4'
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+const KB = {
+    vybor: 'Выбор города',
+    pomosh: 'Поддержка',
+    soc: 'Оставить отзыв',
+    back: 'Назад'
+}
 
 bot.onText(/\/start/, msg => {
 
-    const text = 'Добро пожаловать в магазин VasyaReper, выберите действие в меню ниже'
+    const text = 'Добро пожаловать в магазин vasyareper! Выберите город:'
 
     bot.sendMessage(msg.chat.id, text, {
         reply_markup: {
             keyboard: [
-                [KB.gorod, KB.support, KB.feedback]
+                [KB.vybor, KB.pomosh, KB.soc]
             ]
         }
     })
+})
+
+bot.on('message', msg=> {
+
+    switch (msg.text) {
+        case KB.vybor:
+            break
+        case KB.pomosh:
+            break
+        case KB.soc:
+            break
+        case KB.back:
+            bot.sendMessage(msg.chat.id, 'Выберите пункт меню:', {
+
+            })
+            break
+    }
 
 })
 
-const KB = {
-    gorod: 'Выбор города',
-    support: 'Поддержка',
-    feedback: 'Отзывы',
-    back: 'Назад'
-}
-
-bot.on('message', msg => {
-
-})
-
-switch (msg.text) {
-    case KB.gorod:
-        break
-    case KB.support:
-        break
-    case KB.feedback:
-        break
-    case KB.back:
-        break
-}
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){ res.end('') })
